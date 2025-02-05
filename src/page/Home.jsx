c7196f866767027bf54869f5156e36655611ffbf
SuimKim <github suim0215@gmail.com>
2025-02-06 01:15:35 +0900

    📝 docs: Merge 충돌 수정

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -2,28 +2,12 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { Ball, BtnDex, Logo } from "../style/HomeStyledComponents";
 
-import styled from "styled-components";
-
-const ImgBox = styled.div`
-  display: flex;
-  justify-content: center;
-`;
-
-const BtnDex = styled.button`
-  display: inline-block;
-  height: 50px;
-`;
-
 const Home = () => {
   const navigate = useNavigate();
 
   return (
     <>
       <Logo src="./src/assets/img/logo.svg" alt="" />
-
-      <ImgBox>
-        <img src="./src/assets/pokemon_logo.png" alt="" />
-      </ImgBox>
 
       <BtnDex
         onClick={() => {
--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../page/Home";
 import Dex from "../page/Dex";
-
 import Detail from "../page/Detail";
 
 const Router = () => {
@@ -11,7 +10,6 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dex" element={<Dex />} />
-
         <Route path="/pokemon-detail" element={<Detail />} />
       </Routes>
     </BrowserRouter>
