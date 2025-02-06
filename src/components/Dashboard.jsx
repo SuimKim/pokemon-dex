2eb9408f5c7a813304dfd79568427336856cedc2
SuimKim <github suim0215@gmail.com>
2025-02-06 23:57:48 +0900

    📝 docs: 주석 추가

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -14,7 +14,6 @@ import { swalDeleteAlert, swalToast } from "./SweetAlert";
 const Dashboard = () => {
   const dispatch = useDispatch();
   const myPokemon = useSelector((a) => a.myPokemon);
-  // console.log("result", myPokemon);
 
   const removeMyPokemon = (id) => {
     swalDeleteAlert().then((result) => {
@@ -23,11 +22,12 @@ const Dashboard = () => {
 
         const deletedPokemon = myPokemon.map((a) => {
           return a !== null && a.id === id ? null : a;
-        });
+        }); // 새로운 배열에 선택한 포켓몬을 삭제한 자리를 다시 null로 채움. 원래 null이었던 부분도 마찬가지로 null로 채움.
 
-        const firstNullIndex = deletedPokemon.indexOf(null);
-        const item = deletedPokemon.splice(firstNullIndex, 1);
-        deletedPokemon.splice(6, 0, item[0]);
+        // 빈 카드 뒤로 보내기 위한 정렬 과정
+        const firstNullIndex = deletedPokemon.indexOf(null); // 빈 카드 찾기
+        const item = deletedPokemon.splice(firstNullIndex, 1); // 빈 카드 삭제
+        deletedPokemon.splice(6, 0, item[0]); // 삭제한 빈 카드 맨 뒤에 붙이기
 
         dispatch(setMyPokemon(deletedPokemon));
         localStorage.setItem("pokemon", JSON.stringify(deletedPokemon));
