45b7714ca3d34c6e54144b58046ff4be52ccb362
SuimKim <github suim0215@gmail.com>
2025-02-07 17:35:18 +0900

    ✨ feat(read): 리스트 검색기능 추가

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -6,18 +6,48 @@ import {
   Input,
   BtnBox,
 } from "../style/SearchBoxStyledComponents";
-const SearchBox = () => {
+import { useDispatch } from "react-redux";
+import { useRef } from "react";
+import { setPokemonList } from "../redux/pokeListSlice";
+import { useSelector } from "react-redux";
+const SearchBox = ({}) => {
+  const pokemonList = useSelector((a) => a.pokeListSlice);
+  const dispatch = useDispatch();
+  const searchValueRef = useRef(null);
+  const typeValueRef = useRef(null);
+
+  const searchHandler = (type) => {
+    let searchList = [];
+
+    switch (typeValueRef.current.value) {
+      case "name":
+        searchList = pokemonList.filter((pokemon) => {
+          return pokemon.korean_name.includes(searchValueRef.current.value);
+        });
+      // case "id":
+      //   searchList = pokemonList.filter((pokemon) => {
+      //     return pokemon.id.includes(searchValueRef.current.value);
+      //   });
+      case "types":
+        searchList = pokemonList.filter((pokemon) => {
+          return pokemon.types.includes(searchValueRef.current.value);
+        });
+    }
+
+    dispatch(setPokemonList(searchList));
+  };
+
   return (
     <>
       <Background>
-        <Select name="searchValues">
-          <option value="korean_name">이름</option>
-          <option value="id">Number</option>
+        <Select name="searchValues" ref={typeValueRef}>
+          <option value="name">이름</option>
+          <option value="num">Number</option>
           <option value="types">속성</option>
         </Select>
         <InputBox>
-          <Input type="text" placeholder="포켓몬 입력!" />
-          <BtnBox>
+          <Input type="text" ref={searchValueRef} placeholder="포켓몬 입력!" />
+          <BtnBox onClick={searchHandler}>
             <img src="./src/assets/img/search.png" alt="" />
           </BtnBox>
         </InputBox>
--- src/page/Dex.jsx
+++ src/page/Dex.jsx
@@ -1,19 +1,16 @@
 import React from "react";
 import PokemonList from "../components/PokemonList";
 import Dashboard from "../components/Dashboard";
-import { BackBox, InnerLine, Logo } from "../style/DexStyledComponents";
+import { BackBox, Logo } from "../style/DexStyledComponents";
 import { store } from "../redux/store";
 import { Provider } from "react-redux";
-import SearchBox from "../components/SearchBox";
 
 const Dex = () => {
   return (
     <Provider store={store}>
       <Logo src="./src/assets/img/logo.svg" alt="" />{" "}
       <BackBox>
-        <SearchBox />
         <Dashboard />
-        <InnerLine />
         <PokemonList />
       </BackBox>
     </Provider>
